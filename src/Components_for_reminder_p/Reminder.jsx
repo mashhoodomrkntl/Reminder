b3c59cd35cd17e4e92a0a4338956ebc6c68c2240
Mashhood <mashhoodakomr@gmail.com>
2024-12-14 22:12:22 +0530

    ini.dlt-btn { commit

--- src/Components_for_reminder_p/Reminder.jsx
+++ src/Components_for_reminder_p/Reminder.jsx
@@ -3,30 +3,46 @@ import "./Reminder.css";
 const Reminder = () => {
   const [reminder, setReminder] = useState([]);
   const [newReminder, setNewReminder] = useState();
-  
+
   const handleInputChange = (e) => {
     setNewReminder(e.target.value);
   };
+
   const handleReminder = () => {
     if (newReminder.trim()) {
       setReminder([...reminder, newReminder]);
+      setNewReminder("");
     }
+  };
+  const handleDelte = (index) => {
+    setReminder(reminder.filter((item, itemIndex) => itemIndex != index));
   };
   return (
     <div className="container">
       <h2>Reminder App</h2>
       <div className="input-container">
         <input
+          value={newReminder}
           onChange={handleInputChange}
           placeholder="Enter a reminder "
           type="text"
         />
         <button onClick={handleReminder}>Add Reminder</button>
-        {reminder.map((value) => {
-          console.log(value);
-          <h1>{value}</h1>;
-        })}
       </div>
+      {reminder.length > 0 ? (
+        <ul className="reminder-list">
+          {reminder.map((value, index) => {
+            return (
+              <li key={index}>
+                {value}
+                <button className="dlt-btn" onClick={() => handleDelte(index)}>Delete</button>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>ooops No Reminders !!!</p>
+      )}
     </div>
   );
 };
