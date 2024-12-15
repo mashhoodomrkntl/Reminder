5e5b477d8426a41df354a7b3debdd67c31c5a874
Mashhood <mashhoodakomr@gmail.com>
2024-12-15 10:12:31 +0530

    integrate localstorage

--- src/Components_for_reminder_p/Reminder.jsx
+++ src/Components_for_reminder_p/Reminder.jsx
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Reminder.css";
+
 const Reminder = () => {
-  const [reminder, setReminder] = useState([]);
-  const [newReminder, setNewReminder] = useState();
+  const [reminder, setReminder] = useState(() => {
+    const storedReminders = localStorage.getItem("reminders");
+    return storedReminders ? JSON.parse(storedReminders) : [];
+  });
+
+  const [newReminder, setNewReminder] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("reminders", JSON.stringify(reminder));
+  }, [reminder]);
 
   const handleInputChange = (e) => {
     setNewReminder(e.target.value);
@@ -14,9 +23,11 @@ const Reminder = () => {
       setNewReminder("");
     }
   };
-  const handleDelte = (index) => {
-    setReminder(reminder.filter((item, itemIndex) => itemIndex != index));
+
+  const handleDelete = (index) => {
+    setReminder(reminder.filter((_, itemIndex) => itemIndex !== index));
   };
+
   return (
     <div className="container">
       <h2>Reminder App</h2>
@@ -24,24 +35,24 @@ const Reminder = () => {
         <input
           value={newReminder}
           onChange={handleInputChange}
-          placeholder="Enter a reminder "
+          placeholder="Enter a reminder"
           type="text"
         />
         <button onClick={handleReminder}>Add Reminder</button>
       </div>
       {reminder.length > 0 ? (
         <ul className="reminder-list">
-          {reminder.map((value, index) => {
-            return (
-              <li key={index}>
-                {value}
-                <button className="dlt-btn" onClick={() => handleDelte(index)}>Delete</button>
-              </li>
-            );
-          })}
+          {reminder.map((value, index) => (
+            <li key={index}>
+              {value}
+              <button className="dlt-btn" onClick={() => handleDelete(index)}>
+                Delete
+              </button>
+            </li>
+          ))}
         </ul>
       ) : (
-        <p>ooops No Reminders !!!</p>
+        <p>No Reminders Yet!</p>
       )}
     </div>
   );
